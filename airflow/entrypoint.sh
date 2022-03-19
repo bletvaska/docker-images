58d97fb0af9829731904d562f4f50c636f4eb6f1
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-03-19 15:58:11 +0100

    update of airflow image

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
 
-if [ "$1" = 'airflow' ]; then
+# runtime Python package installations
+if [[ -f /airflow/requirements.txt ]]; then
+    echo "Installing additional packages based on requirements.txt file:"
+    cat /airflow/requirements.txt
+    echo 
+    pip install -r /airflow/requirements.txt
+fi
+
+
+# runtime ubuntu package installation
+if [[ -n $AIRFLOW_PACKAGES ]]; then
+    echo "Installing additional system packages: ${AIRFLOW_PACKAGES}\n"
+    apt install -y "${AIRFLOW_PACKAGES}"
+fi
+
+
+# init environment
+if [[ "$1" = 'airflow' ]]; then
     # check if db is initialized
-    airflow db check-migrations || {
+    airflow db check-migrations 2> /dev/null || {
         airflow db init
         airflow users create \
-            --username admin \
-            --password admin \
+            --username "${AIRFLOW_USER:-admin}" \
+            --password "${AIRFLOW_PASSWORD:-admin}" \
             --firstname John \
             --lastname Doe \
             --role Admin \
@@ -18,6 +35,7 @@ if [ "$1" = 'airflow' ]; then
     [[ -d /airflow/dags ]] || mkdir /airflow/dags
     [[ -d /airflow/plugins ]] || mkdir /airflow/plugins
 fi
+
 
 # exec
 exec "$@"
