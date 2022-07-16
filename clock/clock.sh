73eaf71df20eda90715099d96a2c14a61b17bbbd
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-07-16 21:16:40 +0200

    clock refactoring: clock script currently reacts on signals INTERRUPT and TERMINATE

--- clock/clock.sh
+++ clock/clock.sh
@@ -1,31 +1,44 @@
 #!/usr/bin/env sh
 
-function on_exit(){
-    echo "on_exit"
-    signal="ON EXIT"
-}
-
 
 function on_sigint(){
-    echo "on_sigint"
     signal="INTERRUPT"
+    timer=5
 }
 
 
 function on_sigterm(){
-    echo "on_sigterm"
     signal="TERMINATE"
+    timer=5
 }
 
-trap on_exit EXIT
-trap on_sigint SIGINT
-trap on_sigint SIGTERM
 
-while true; do 
+trap on_sigint  SIGINT   # 2
+trap on_sigterm SIGTERM  # 15
+
+
+function center(){
+    text=$1
+    cols=$(tput cols)
+    printf "%"$(( (cols + ${#text})/2))"s" "${text}"
+}
+
+while true; do
+    # render
     clear
     date "+%H:%M:%S" | figlet -ctk
-    if [[ ${signal} ]]; then
-        echo "${signal}"
+
+    # if signal was received, then timer is running
+    if [ "${timer}" ]; then
+        center "Signal ${signal} Received."
+        timer=$(( ${timer} - 1 ))
+
+        # remove timer when on 0
+        if [ "${timer}" -eq 0 ]; then
+            unset timer
+        fi
     fi
+
+    # sleep
     sleep 1
 done
